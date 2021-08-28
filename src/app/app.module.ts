7bff79043a74d10e23ab37b805f7e691008acaaa
Samhitha2609 <samhithareddyk@hexaware.com>
2021-08-28 21:55:24 +0530

    5 files modified, 2 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,11 @@ import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { TopBarComponent } from './top-bar/top-bar.component';
 import { ProductListComponent } from './product-list/product-list.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent, TopBarComponent, ProductListComponent ],
+  declarations: [ AppComponent, HelloComponent, TopBarComponent, ProductListComponent, SignUpComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
